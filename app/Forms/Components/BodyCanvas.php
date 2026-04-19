5272a36d082eae3ce1a87df6f3b67d39824c4e96
damon <chenzifeng1987@gmail.com>
2026-04-19 20:21:50 +0800

    fix: 移除BodyCanvas组件的自定义make()方法

--- app/Forms/Components/BodyCanvas.php
+++ app/Forms/Components/BodyCanvas.php
@@ -7,12 +7,4 @@ use Filament\Forms\Components\Field;
 class BodyCanvas extends Field
 {
     protected string $view = 'filament.forms.components.body-canvas';
-
-    public static function make(string $name): static
-    {
-        $static = app(static::class);
-        $static->name($name);
-        $static->configure();
-        return $static;
-    }
 }
