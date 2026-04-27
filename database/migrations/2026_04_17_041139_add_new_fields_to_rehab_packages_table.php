25f36e6e0098df2af803caa5d6c1e5fdf7f5abf5
damon <chenzifeng1987@qq.com>
2026-04-27 17:36:04 +0800

    fix duplicate issue

--- database/migrations/2026_04_17_041139_add_new_fields_to_rehab_packages_table.php
+++ database/migrations/2026_04_17_041139_add_new_fields_to_rehab_packages_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::table('rehab_packages', function (Blueprint $table) {
 //            $table->string('package_type', 50)->default('single')->comment('套餐类型：single-单次, course-疗程卡, monthly-月卡, quarterly-季卡, special-特惠次卡, item-单项服务')->after('status');
-            $table->decimal('original_price', 10, 2)->default(0)->comment('原始价格')->after('package_type');
+//            $table->decimal('original_price', 10, 2)->default(0)->comment('原始价格')->after('package_type');
             $table->decimal('average_price', 10, 2)->default(0)->comment('均价')->after('original_price');
             $table->boolean('is_extendable')->default(false)->comment('是否可延期')->after('average_price');
             $table->integer('extension_days')->default(0)->comment('可延期天数')->after('is_extendable');
